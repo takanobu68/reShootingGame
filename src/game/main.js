cba3ea9c66febb06450624bef68db02d62dc623c
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-31 21:55:39 +0900

    登場時画面の左側から徐々に登場

--- src/game/main.js
+++ src/game/main.js
@@ -1,4 +1,5 @@
 import { Canvas2D } from "./common/Canvas2D.js";
+import { Player } from "./character/Player.js";
 
 export function createGame() {
   /**
@@ -29,21 +30,18 @@ export function createGame() {
    * @type {Image}
    */
   let image;
+
   /**
    * 実行開始時のタイムスタンプ
    * @type {number}
    */
   let startTime;
+
   /**
-   * player の X 座標
-   * @type {number}
+   * 自機キャラクターのインスタンス
+   * @type {Player}
    */
-  let playerX = GAME_CONTAINER.clientWidth / 2;
-  /**
-   * player の Y 座標
-   * @type {number}
-   */
-  let playerY = GAME_CONTAINER.clientHeight / 2;
+  let player = null;
 
   // Canvas2Dクラスの初期化
   util = new Canvas2D(GAME_CONTAINER, GAME_CANVAS);
@@ -70,27 +68,32 @@ export function createGame() {
   /**
    * 初期化関数
    */
-  function initialize() {}
+  function initialize() {
+    // 自機キャラクターを初期化する
+    player = new Player(ctx, 0, 0, image);
+
+    player.setComing(-100, canvas.height / 2, 0, canvas.height / 2);
+  }
 
   /**
    * イベントを設定する関数を定義
    */
   function eventSetting() {
     // キーの押下時に呼び出されるイベントリスナーを設定する
-    window.addEventListener("keydown", (event) => {
+    window.addEventListener("keydown", (e) => {
       // 入力されたキーに応じて処理内容を変化させる
-      switch (event.key) {
+      switch (e.key) {
         case "ArrowLeft": // アローキーの左
-          playerX -= 10;
+          player.position.x -= 10;
           break;
         case "ArrowRight": // アローキーの右
-          playerX += 10;
+          player.position.x += 10;
           break;
         case "ArrowUp":
-          playerY -= 10; // アローキーの上
+          player.position.y -= 10; // アローキーの上
           break;
         case "ArrowDown":
-          playerY += 10; // アローキーの下
+          player.position.y += 10; // アローキーの下
           break;
       }
     });
@@ -100,9 +103,32 @@ export function createGame() {
    * 描画処理を行う関数を定義
    */
   function render() {
+    // グローバルなアルファを必ず 1.0 で描画処理を開始する
+    ctx.globalAlpha = 1.0;
     util.drawRect(0, 0, canvas.width, canvas.height, "#000");
+
+    // 登場シーンの処理
+    if (player.isComing === true) {
+      // 登場シーンが始まってからの経過時間
+      let justTime = Date.now();
+      let comingTime = (justTime - player.comingStart) / 1000;
+      // 登場中は時間が経つほど右に向かって進む
+      let x = -100 + comingTime * 50;
+      // 一定の位置まで移動したら登場シーンを終了する
+      if (x >= player.comingEndPosition.x) {
+        player.isComing = false; // 登場シーンフラグを下ろす
+        x = player.comingEndPosition.x; // 行き過ぎの可能性もあるので位置を再設定
+      }
+      // 求めたX座標を自機に設定する
+      player.position.set(x, player.position.y);
+      // justTime を 100 で割ったとき余りが 50 より小さくなる場合だけ半透明にする
+      if (justTime % 100 < 50) {
+        ctx.globalAlpha = 0.5;
+      }
+    }
+
     // 画像を描画する
-    ctx.drawImage(image, playerX, playerY);
+    player.draw();
     // 描画処理を再帰呼出しする
     requestAnimationFrame(render);
   }
