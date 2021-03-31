824012176a0aa877161279ac6a750c2fd58101a2
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-03-31 22:32:08 +0900

    プロパティの追加とdrawメソッドの修正

--- src/game/common/CharacterBase.js
+++ src/game/common/CharacterBase.js
@@ -9,19 +9,33 @@ export class CharacterBase {
    * @param {number} life - 生存フラグ
    * @param {Image} image - キャラクターの画像
    */
-  constructor(ctx, x, y, life, image) {
+  constructor(ctx, x, y, width, height, life, image) {
     /**
      * @type {CanvasRenderingContext2D}
      */
     this.ctx = ctx;
+
     /**
      * @type {Position}
      */
     this.position = new Vector2(x, y);
+
+    /**
+     * @type{number}
+     */
+    this.width = width;
+
     /**
      * @type {number}
      */
+    this.height = height;
+
+    /**
+     *
+     * @type {number}
+     */
     this.life = life;
+
     /**
      * @type {Image}
      */
@@ -32,6 +46,16 @@ export class CharacterBase {
    * キャラクターを描画する
    */
   draw() {
-    this.ctx.drawImage(this.image, this.position.x, this.position.y);
+    // キャラクターの幅を考慮してオフセットする量
+    const offsetY = this.height / 2;
+
+    // キャラクターの幅やオフセットする量を加味して描画する
+    this.ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y - offsetY,
+      this.width,
+      this.height
+    );
   }
 }
