adbfdc387d38c38c191596deb3ab64590b5d0238
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-04-01 14:27:51 +0900

    Shotクラスのファイル作成

---
+++ src/game/character/Shot.js
@@ -0,0 +1,52 @@
+import { CharactorBase } from "../common/CharacterBase.js}";
+
+export class Shot extends CharactorBase {
+  /**
+   * @constructor
+   * @param {CanvasRenderingContext2D} ctx
+   * @param {number} x
+   * @param {number} y
+   * @param {number} w
+   * @param {number} h
+   * @param {image} image
+   */
+  constructor(ctx, x, y, w, h, image) {
+    // 継承元の初期化
+    super(ctx, x, y, w, h, 0, image);
+    /**
+     * 自身の移動スピード(update1回あたりの移動量)
+     * @type{number}
+     */
+    this.speed = 7;
+  }
+
+  /**
+   * ショットを配置する
+   * @param{number} x
+   * @param{number} y
+   */
+  set(x, y) {
+    // 登場配置位置にショットを移動させる
+    this.position.set(x, y);
+    // ショットのライフを0より大きい値に設定する
+    this.life = 1;
+  }
+
+  /**
+   * キャラクターの状態を更新し描画も行う
+   */
+  update() {
+    // もしショットのライフが0以下の場合、何もしない
+    if (this.life <= 0) {
+      return;
+    }
+    // もしショットが画面外へ移動していたらライフを0に設定する
+    if (this.position.x + this.width > this.ctx.canvas.width) {
+      this.life = 0;
+    }
+    // ショットを右に向かって移動させる
+    this.position.x += this.speed;
+    // ショットを描画する
+    this.draw();
+  }
+}
