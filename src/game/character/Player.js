e91b6b3f08a43d2d30cd39dcbfd096b16916a696
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-04-01 17:34:47 +0900

    zを押した時の処理を追加 checkcounterを基準にして、ショットを打てるようにif文を修正

--- src/game/character/Player.js
+++ src/game/character/Player.js
@@ -124,15 +124,23 @@ export class Player extends CharacterBase {
 
       // キーの押下状態を調べてショットを生成する
       if (window.isKeyDown.key_z === true) {
-        // ショットの生存を確認し非生存の物があれば生成する
-        for (let i = 0; i < this.shotArray.length; i++) {
-          if (this.shotArray[i].life <= 0) {
-            this.shotArray[i].set(this.position.x, this.position.y);
-            // 1つ生成したらループを抜ける
-            break;
+        // ショットを打てる状態なのか確認する
+        // ショットチェック用カウンターが0以上ならショットを生成できる
+        if (this.shotCheckCounter >= 0) {
+          // ショットの生存を確認し非生存の物があれば生成する
+          for (let i = 0; i < this.shotArray.length; i++) {
+            if (this.shotArray[i].life <= 0) {
+              this.shotArray[i].set(this.position.x, this.position.y);
+              // ショットを生成したのでインターバルを設定する
+              this.shotCheckCounter = -this.shotInterval;
+              // 1つ生成したらループを抜ける
+              break;
+            }
           }
         }
       }
+      // ショットチェック用のカウンターをインクリメントする
+      ++this.shotCheckCounter;
     }
     // 自機キャラクターを描画する
     this.draw();
