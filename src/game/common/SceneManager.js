ab1a8ef57d9833eab24d24b0e2b8911e0231cecd
Takanobu <61861946+takanobu68@users.noreply.github.com>
2021-04-04 17:36:44 +0900

    SceneManagerクラスを作成

---
+++ src/game/common/SceneManager.js
@@ -0,0 +1,65 @@
+export class SceneManager {
+  constructor() {
+    /**
+     * シーンを格納するためのオブジェクト
+     * @type{object}
+     */
+    this.scene = {};
+
+    /**
+     * 現在アクティブなシーン
+     * @type{function}
+     */
+    this.activeScene = null;
+
+    /**
+     * 現在のシーンがアクティブになった時刻のタイムスタンプ
+     * @type{number}
+     */
+    this.startTime = null;
+
+    /**
+     * 現在のシーンがアクティブになってからのシーンの実行回数
+     * @type{number}
+     */
+    this.frame = -1;
+  }
+
+  /**
+   * シーンを追加する
+   * @param{string} name - シーンの名前
+   * @param{function} updateFunction - シーン中の処理
+   */
+  add(name, updateFunction) {
+    this.scene[name] = updateFunction;
+  }
+
+  /**
+   * アクティブなシーンを設定する
+   * @param{string} name - アクティブにするシーンの名前
+   */
+  use(name) {
+    // 指定されたシーンが存在するか確認する
+    if (!this.scene.hasOwnProperty(name)) {
+      return;
+    }
+    // 名前を元にアクティブなシーンを設定する
+    this.activeScene = this.scene[name];
+    // シーンをアクティブにした瞬間のタイムスタンプを設定する
+    this.startTime = Date.now();
+    // シーンをアクティブにしたのでカウンターをリセットする
+    this.frame = -1;
+  }
+
+  /**
+   * シーンを更新する
+   */
+  update() {
+    // シーンがアクティブになってからの経過時間
+    let activeTime = (Date.now() - this.startTime) / 1000;
+    //経過時間を引数に与えてupdateFunctionを呼び出す
+    this.activeScene(activeTime);
+    //シーンを更新したのでカウンターをインクリメントする
+    this.frame++;
+  }
+}
